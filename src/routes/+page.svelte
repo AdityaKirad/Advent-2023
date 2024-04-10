1856ccd9d02c8b0b75e1df81960608b10abee4f5
Aditya <kiradaditya2001@gmail.com>
2024-04-10 21:17:25 +0530

    refactor: hompage layout updated

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,42 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import { Button } from 'flowbite-svelte';
+
+  type ChallengeRoutes = Array<{
+    title: string;
+    description: string;
+    href: string;
+  }>;
+
+  const routes: ChallengeRoutes = [
+    {
+      title: 'Day 1: Naughty or Nice',
+      description:
+        'Elves modern system track whether children are Naughty or Nice based on thier deeds.',
+      href: '/day1'
+    },
+    {
+      title: 'Day 2: Merry Munch-o-Meter',
+      description: 'Cookie counter to track cookies munched by the Santa',
+      href: '/day2'
+    }
+  ];
+</script>
+
+<svelte:head>
+  <title>Advent Of Svelte 2023</title>
+  <meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<main class="mt-12 flex flex-col gap-4">
+  {#each routes as { title, description, href }}
+    <div class="space-y-2 text-center">
+      <h3 class="text-xl font-semibold">{title}</h3>
+      <p>{description}</p>
+      <a
+        class="inline-block rounded border-2 border-solid px-4 py-2 hover:bg-white/10 focus-visible:bg-white/10"
+        {title}
+        {href}>View Solution</a
+      >
+    </div>
+  {/each}
+</main>
