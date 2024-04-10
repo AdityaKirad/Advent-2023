20f629fa271d729d16b5925e36136dca526b1474
Aditya <kiradaditya2001@gmail.com>
2024-04-10 21:13:47 +0530

    feat(ui library & icons): add flowbite svelte & icons ui library

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,20 @@
 import type { Config } from 'tailwindcss';
 
 export default {
-  content: ['./src/app.html', './src/**/*.svelte'],
+  content: [
+    './src/app.html',
+    './src/**/*.svelte',
+    './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'
+  ],
   theme: {
+    screens: {
+      sm: '40em',
+      md: '48em',
+      lg: '64em',
+      xl: '80em',
+      '2xl': '96em'
+    },
     extend: {}
   },
-  plugins: []
+  plugins: [require('flowbite/plugin')]
 } satisfies Config;
