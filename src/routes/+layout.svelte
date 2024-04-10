3e718850113f2982df2ca443c324f3b01ecd337f
Aditya <kiradaditya2001@gmail.com>
2024-04-10 21:16:04 +0530

    refactor: root layout updated

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,11 @@
 <script>
-  import './style.css';
+  import './styles.css';
 </script>
 
-<slot />
+<div class="p-4 lg:p-8">
+  <h1 class="text-center text-3xl font-bold">Advent of Svelte 2023</h1>
+  <h2 class="text-center text-lg text-zinc-400">
+    A Svelte challenge for each day until Christmas Eve!
+  </h2>
+  <slot />
+</div>
